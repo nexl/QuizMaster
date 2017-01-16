98295de49835ce9ffdc4ad33e842d148a37f3dfe
steven <steven.jun@hotmail.com>
2017-01-17 00:37:15 +0900

    Update user model
    set default role -> student,  add validations for first_name, last_name, email , and password (digest)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   
+  default_value_for :role, 'Student'
+
   has_many :quizz 
   has_many :student_answer
   
+  has_secure_password
+  
+  ALPHABET_ONLY = /[A-Za-z\d]/i
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :first_name, :presence => true, :format => { :with => ALPHABET_ONLY, :message => "Alphabet only"}
+  validates :last_name, :presence => true, :format => { :with => ALPHABET_ONLY, :message =>"Alphabet only"}
+  validates :email, :presence => true, :format => { :with => EMAIL_REGEX, :message => "Email format is wrong" }, :uniqueness => { :message => "This email already registered"}
+  validates :password, :presence => true
+
 end
