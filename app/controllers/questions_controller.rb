7018f21199ce16e7d8c91e9e6fd4c2af074d7240
steven <steven.jun@hotmail.com>
2017-01-17 22:09:32 +0900

    Update - delete question
     plain update - delete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,6 +11,19 @@ class QuestionsController < ApplicationController
     @question ||= Question.new
   end
 
+  def edit 
+    @question ||= Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to quiz_path(@question.quizz_id)
+    else
+      redirect_to root_path
+    end
+  end
+
   def create
     @question = Question.new(question_params)
     if @question.save
@@ -18,6 +31,13 @@ class QuestionsController < ApplicationController
     else
       redirect_to root_path
     end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    quiz = question.quizz_id
+    question.destroy
+    redirect_to quiz_path(quiz)
   end
 
   private
