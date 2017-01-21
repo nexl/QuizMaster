5757305eba4b8a97c57503e74df238cd2864b9a2
steven <steven.jun@hotmail.com>
2017-01-22 02:36:00 +0900

    Add assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  #config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w['*.js', '*.css', '*.css.erb','*.png', '*.jpg', '*.jpeg' ,'*.gif', '*.erb', '*.svg', '*.scss', '*.sass', '*.svg', '*.eot', '*.woff', '*.ttf']
+  config.public_file_server.enabled = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
