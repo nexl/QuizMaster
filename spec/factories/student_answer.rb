3cf706859bb2f0369a090a3af76dfe91245db056
steven <steven.jun@hotmail.com>
2017-01-22 00:24:41 +0900

    Update student_answer factories

--- spec/factories/student_answer.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :student_answer_true do
-    answer  "trump"
-  end
-
-  factory :student_answer_false do
-    answer  "clinton"
-  end
-end
---
+++ spec/factories/student_answers.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :student_answer do
+    answer  "magneto"
+  end
+end
