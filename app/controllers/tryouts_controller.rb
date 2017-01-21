dc2345c1feaa9dcc2102318f5879cc46826be46e
steven <steven.jun@hotmail.com>
2017-01-21 22:19:54 +0900

    Remove unused code
    Tryout was moved to api and react

--- app/controllers/tryouts_controller.rb
+++ app/controllers/tryouts_controller.rb
@@ -1,39 +1,5 @@
 class TryoutsController < ApplicationController
   
-  def show
-    # render component: 'Quiz', props: { quiz_id: params[:quizz_id], token: params[:token] }
-  end
-
-  def create
-    question = Question.find(params[:student_answer][:question_id])
-    if question.answer == params[:student_answer][:answer] 
-      result = true
-    elsif question.answer.to_i.humanize == params[:student_answer][:answer]
-      result = true
-    else 
-      result = false
-    end
-    @tryout = StudentAnswer.new(tryout_params.merge({:is_right => result, :student_id => session[:user_id]}))
-    if @tryout.save
-      if question.next.present?
-        redirect_to tryout_path(:quizz_id => 10, :question_id => question.next) 
-      else 
-        redirect_to root_path
-      end
-    else
-      Rails.logger.info(@tryout.errors.inspect)
-      redirect_to root_path
-    end
-  end
-
-  private
-
-  def tryout_params
-    params.require(:student_answer).permit(:id, :student_id, :question_id, :answer, :is_right)
-  end
-
-  def integer?(str)
-    !!Integer(str) rescue false
-  end
+  def show; end
 
 end
