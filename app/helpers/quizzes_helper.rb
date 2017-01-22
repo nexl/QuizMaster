101fe65577fe8b821a9485fbfb2f796041f2468f
steven <steven.jun@hotmail.com>
2017-01-22 13:27:28 +0900

    Page for user who has no quiz

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,10 @@
 module QuizzesHelper
+  
+  def new_quiz
+    link_to "create a new one?", new_quiz_path 
+  end
+
+  def quiz_detail(quiz_id)
+    link_to "Detail", quiz_path(quiz_id), { :class => "btn btn-sm btn-default" }
+  end
 end
