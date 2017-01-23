b26d2607f78cd79167c5484e72ee3dffae0e62ad
steven <steven.jun@hotmail.com>
2017-01-23 23:21:54 +0900

    Fix bug : remove students
    before : always remove user at index 0

--- app/assets/javascripts/components/_student_list_detail.es6.jsx
+++ app/assets/javascripts/components/_student_list_detail.es6.jsx
@@ -5,7 +5,7 @@ const StudentListDetail = ({full_name, link_id, token, handleDelete , index}) =>
     <tr>
       <td>{ full_name } </td>
       <td><a target="_blank" href={`../tryouts/${link_id}/${token}`} className="btn btn-default btn-sm">Tryout</a></td>
-      <td><button onClick={ index => handleDelete(index) }  type="button" className="btn btn-danger btn-sm">Delete { full_name }</button></td>
+      <td><button onClick={ e => handleDelete(index) }  type="button" className="btn btn-danger btn-sm">Delete { full_name }</button></td>
     </tr>
   )
 }
