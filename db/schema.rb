704e205341ea281ae9af59bf345df4573307f195
steven <steven.jun@hotmail.com>
2017-01-24 01:04:47 +0900

    Remove role from users
    unused column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119122111) do
+ActiveRecord::Schema.define(version: 20170123155950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20170119122111) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "role"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
